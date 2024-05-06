672a9d2882d70474c33a00ab8fe6e824b96926b0
hardikAhj <ahujavansh96@gmail.com>
2024-05-06 20:57:36 +0530

    added database

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -41,6 +41,7 @@ export async function POST(req: Request) {
 
   // Verify the payload with the headers
   try {
+    console.log("verifying webhook")
     evt = wh.verify(body, {
       "svix-id": svix_id,
       "svix-timestamp": svix_timestamp,
